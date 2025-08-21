d21623335ad5457761ee99347c60e73e2257965e
bu11ymaguire <134497208+bu11ymaguire@users.noreply.github.com>
2025-08-21 15:16:39 +0900

    Update fahrenheit.py

--- fahrenheit.py
+++ fahrenheit.py
@@ -1,5 +1,10 @@
-//본 프로그램은 섭씨를 화씨로 변환해주는 프로그램입니다.
-//변환하고 싶은 섭씨 온도를 입력해 주세요:
+def Celsius_to_Ferh(x):
+      return x * 1.8 + 32
 
-//섭씨 온도:
-//화씨 온도:
+print("본 프로그램은 섭씨 온도를 화씨 온도로 변환해주는 프로그램입니다.")
+cel = input("변환하고 싶은 섭씨 온도를 입력해 주세요: ")
+celS = float(cel)
+ferh = Celsius_to_Ferh(celS)
+
+print(f"섭씨 온도: {celS}")
+print(f"화씨 온도: {ferh}")
